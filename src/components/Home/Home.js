3fec73210ea8f7f655c044e00b40035993148e93
jhuangliu <95jhliu@gmail.com>
2021-11-01 10:16:40 -0400

    new npm installed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect} from 'react'; 
 
 const Home = () =>{
-	const [home, setHome] = useState();
-	const [quote, setQuote] = useState();
-	const [click, setClick] = useState(false);
-	const [quoteClick, setQuoteClick] = useState(true);
-	const [index, setIndex] = useState(0);
+
 
 
 return (
